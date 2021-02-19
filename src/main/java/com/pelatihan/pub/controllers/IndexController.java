743a37dc3271f0573f23d8a9022e18a942ea6684
mkadjg <saputra.sndi@gmail.com>
2021-02-19 14:37:12 +0700

    menambah setting khusus deploy ke heroku

---
+++ src/main/java/com/pelatihan/pub/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.pelatihan.pub.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.view.RedirectView;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public RedirectView index(){
+        return new RedirectView("/home/");
+    }
+
+}
